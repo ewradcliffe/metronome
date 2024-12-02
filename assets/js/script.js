9eacdb56749fa70bf5ab79f6adf4e2e963ec4a4a
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-12-02 16:32:02 +0000

    Add function to decrease bpm when '-' button clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Loads default bpm
   let bpm = 100;
-  document.getElementById('bpm-count').innerHTML = bpm;
+  let bpmCount = document.getElementById('bpm-count')
+  bpmCount.innerHTML = bpm;
+  
+
+  // function to decrease bpm by one.
+  function decreaseBPM() {
+    bpm -= 1;
+    bpmCount.innerHTML = bpm;
+  }
+
+  // Event Listeners
+  // Decrease Tempo
+  const slowButton = document.getElementById('slow-button');
+  slowButton.addEventListener('click', decreaseBPM);
 });
