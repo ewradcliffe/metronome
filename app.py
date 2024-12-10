8953f4aae4b9b40a0448e5f091127f9686e242d6
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-12-10 20:26:31 +0000

    Add start_metronome function in app.py.

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 # Route for the homepage
 @app.route('/')
 def index():
-    tempo = 100  # This is the value you want to load
+    tempo = 100
     return render_template('index.html', tempo=tempo)
 
+
+@app.route('/start-metronome', methods=['GET'])
+def start_metronome_route():
+    return "Metronome started!"
 
 if __name__ == '__main__':
     app.run(debug=True)
