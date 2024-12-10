a79e81722e5c68d4287273340799df3827e3749f
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-12-10 21:16:05 +0000

    Update JS to accept looped response.

--- static/js/script.js
+++ static/js/script.js
@@ -7,13 +7,16 @@ const lowBeat = new Audio('static/sounds/250552__druminfected__metronome.wav');
 
 // Starts metronome.
 function startMetronome() {
-  fetch('/start-metronome')
-      .then(response => response.text())
-      .then(data => {
-          console.log(data);
-          lowBeat.play();
-      })
-      .catch(error => {
-          console.error('Error:', error);
-      });
+  const eventSource = new EventSource('/start-metronome');
+
+  // Play the metronome sound for every "tick" message received from the server
+  eventSource.onmessage = function(event) {
+      console.log("Message from server:", event.data);
+      lowBeat.play();
+  };
+
+  eventSource.onerror = function(event) {
+      console.error("Error with SSE:", event);
+      eventSource.close();
+  };
 }
