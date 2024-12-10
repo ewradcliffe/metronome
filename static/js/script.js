76655145647547b173166e9c8584ea43edf91602
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-12-10 20:29:39 +0000

    Add JS code to play audio

--- static/js/script.js
+++ static/js/script.js
@@ -3,13 +3,15 @@ window.onload = function() {
   document.getElementById('bpm-count').textContent = bpmCount;
 };
 
+const lowBeat = new Audio('static/sounds/250552__druminfected__metronome.wav');
+
 // Starts metronome.
 function startMetronome() {
   fetch('/start-metronome')
       .then(response => response.text())
       .then(data => {
           console.log(data);
-        
+          lowBeat.play();
       })
       .catch(error => {
           console.error('Error:', error);
